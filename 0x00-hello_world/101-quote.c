6e143bbf1d4dbe399efc54b38402dc1f864f8bc8
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-10 13:45:41 -0800

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Prints out the last part of a quote in the standard error.
+ * main - Prints exactly "and that piece of art is useful"
+ * - Gideon obae, 2021-08-26",
+ * followed by a new line, to the standard error.
  * Return: 1 if success.
  */
 int main(void)
 {
-	write(2, "\and that piece of art is useful" - Dora Korpar, 2015-10-19\n, 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
