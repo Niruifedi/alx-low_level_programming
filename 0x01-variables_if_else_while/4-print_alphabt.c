7533369216190f325d9e4230104870f7f3345355
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-11 11:36:11 -0800

    print with execption

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - mian block
- * Description: print alphabets in lowercase and uppercase with the exception of 'q' and 'e'.
+ * Description: print alphabets in lowercase and uppercase with 
+ * the exception of 'q' and 'e'.
  * Return: 0
  */
 int main(void)
