8378366275d51ad943cd43af3f90a73ffba13736
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-11 15:04:28 -0800

    betty pls

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
  * main - mian block
- * Description: print alphabets in 
- * lowercase and uppercase with 
+ * Description: print alphabets in
+ * lowercase and uppercase with
  * the exception of 'q' and 'e'.
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	char c = 'a';
-	
+
 	while (c <= 'z')
 	{
 		if (c != 'e' && c != 'q')
