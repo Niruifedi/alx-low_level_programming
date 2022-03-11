2939d87adcf1d156e2a16a35b924609bb6cd917f
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-11 08:09:24 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: print out alphabets in lower case, followed by  new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	
+	{
+		putchar(c);
+
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
