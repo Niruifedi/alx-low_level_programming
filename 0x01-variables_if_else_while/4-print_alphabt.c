d8a137a279d7d9c2411fb12c16ac0e4cbf26a294
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-11 11:33:18 -0800

    print with execption

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - mian block
+ * Description: print alphabets in lowercase and uppercase with the exception of 'q' and 'e'.
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
