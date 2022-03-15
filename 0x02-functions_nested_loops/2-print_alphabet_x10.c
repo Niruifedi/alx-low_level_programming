40051f200bca9d18426c4e076db1ba9bb101d5d8
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-15 08:42:48 -0700

    10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print alphabet - print alphabet x10
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int k, go;
+
+	go = 0;
+
+	while (go < 10)
+	{
+		for (k = 'a'; k <='z'; k++)
+		{
+			_putchar(k);
+		}
+		go++;
+
+		_putchar('\n');
+	}
+
+	return (0);
+}
