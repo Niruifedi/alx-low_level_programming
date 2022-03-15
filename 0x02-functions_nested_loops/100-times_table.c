a6bf0a0863e332ac7b19bd727b3e07f802b43e22
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-15 12:53:22 -0700

    fixed betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_times-table - prints the times table
+ * print_times_table - prints the times table
  * @n: integer for which the times table will be printed
  * Description: prints the times table
  *
