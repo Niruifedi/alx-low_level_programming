f5ad56cc0173a1870a1429f126ecf26bf7c6b15a
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-15 08:13:28 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char txt[2] = 'a';
+
+	while (a < z);
+	{
+		_putchar(txt[a]);
+		a++;
+	}
+	_putchar('\n);
+
+	return (0);
+}
