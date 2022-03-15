4a1eaffc290501788662cdc5c900e874eba7fd1e
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-15 12:43:55 -0700

    times tablee

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_times-table - prints the times table
+ * @n: integer for which the times table will be printed
+ *
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int row, column, product;
+
+	if (n >= 0 && n < 15)
+	{
+		for (row = o; row <= n; row++)
+		{
+			for (column = o; column <= n; column++)
+			{
+				product = row * column;
+
+				if (column == 0)
+					_putchar('0');
+				else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
