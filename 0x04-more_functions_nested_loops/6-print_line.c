592ea635ff09af7b0dfac5adf0703ab5537263e8
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-17 14:14:03 -0700

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - a function thta drwas a straight line in the terminal.
+ * followed by a newline
+ * @n: an input integer
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > o)
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
