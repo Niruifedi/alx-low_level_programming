d1702892004998a41ab7cb76348d5a635a53d00d
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-17 04:57:50 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - using is upper function
+ * _isupper: this fucntion checks for uppercase character and prints it out
+ * @c: c is an ascii value
+ * Return: 1 if c is uppercase.
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 'A';
+	_putchar(c, _isupper(c));
+
+	c = 'a';
+	_putchar(c, _isupper(c));
+
+	return (0);
+}
+
