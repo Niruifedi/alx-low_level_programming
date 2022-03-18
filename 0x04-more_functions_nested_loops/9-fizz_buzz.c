1ff2e41231307e2cb4353c4e4929c36b53b1ea6c
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-17 17:28:23 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,16 +15,25 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("%s", "FizzBuzz");
-		else if (i % 3 == 0)
-			printf("%s", "Fizz");
-		else if (i % 5 == 0)
-			printf("%s", "Buzz ");
-		else
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 ==0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i == 1)
+		{
 			printf("%d ", i);
-		if (i != 100)
-			printf(" ");
+		}
+		else 
+		{
+			printf(" %d", i)
 	}
 	printf("\n");
 
