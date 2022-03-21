bbb83d66d735b04a76d8eda3468183f362da8c03
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-21 12:45:15 -0700

    forward

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse followed by a newline
+ * @s: pointer to get string from
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i--
+	}
+	_putchar('\n');
+}
