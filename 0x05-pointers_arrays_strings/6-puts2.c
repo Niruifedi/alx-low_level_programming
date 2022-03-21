e231f99a63998df216695189951d56c1c9f831c9
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-21 14:18:39 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer to the string to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+
+		i++;
+	}
+	_putchar('\n');
+}
