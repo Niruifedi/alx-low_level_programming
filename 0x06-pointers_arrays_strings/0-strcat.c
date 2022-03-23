ae639d8ede3462937c26ec2083660c380ef5ae1c
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-23 16:32:19 -0700

    betty fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 #include "string.h"
 
 /**
- * _strcat(char *dest, char *src) - this function appends two strings together
+ * _strcat - this function appends two strings together
  * overwriting the null terminate (\0) of dest.
  * @dest: destination string to append to.
  * @src: string to append.
