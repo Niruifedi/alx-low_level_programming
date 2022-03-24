b933962624ffd407cda6e15e7b5fb3436ed7faa5
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-24 13:43:06 -0700

    print num

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,44 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	int power;
+	int neg;
+	int hold;
+
+	neg = 0;
+	power = 1;
+	hold = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		neg = 1;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	while (hold > 9 || hold < -9)
+	{
+		power *= 10;
+		hold /= 10;
+	}
 
-	_putchar((num % 10) + '0');
+	while (power > 0)
+	{
+		if (power > 9)
+		{
+			if (!neg)
+				_putchar((n / power % 10) + '0');
+			else
+				_putchar((n / power % 10) * -1 + '0');
+
+			power /= 10;
+		}
+		if (power == 1)
+		{
+			if (neg)
+				_putchar((n % 10) * -1 + '0');
+			else
+				_putchar(n % 10 + '0');
+			power = 0;
+		}
+	}
 }
