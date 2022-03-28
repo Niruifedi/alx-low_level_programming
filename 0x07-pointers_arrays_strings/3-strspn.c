e70319990d5408cc4d7bf21de786d4c1948b829f
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-28 09:38:05 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - function thaty gets the lenght of a prefix substring
+ * @s: string to be scanned
+ * @accept: string containing the list of characters to match
+ * Return: int from s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0;
+
+	if ((s == NULL) || (accept == NULL))
+		return len;
+
+	while (*s && strchr(accept, *s++))
+	{
+		len++;
+	}
+
+	return (len);
+}
