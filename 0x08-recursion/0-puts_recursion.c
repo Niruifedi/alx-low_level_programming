29e71e38d0a6787d6dd1bf82bf4df2aa218ac856
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-03-30 08:16:43 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string with a new line
+ * @s: variable to print strings from
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
