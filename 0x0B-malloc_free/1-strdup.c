878fbc9ce323aa4b755da105f6713f88a2cb2bad
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-04 12:01:19 -0700

    fixed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,7 +4,6 @@
 /**
  * _strdup - returns pointer to a new string duplicate of str
  * @str: string to get duplicate form
- * @dest: pointer of new string duplicate
  * Return: NULL if str is NULL, on success return pointer to the string
  */
 char *_strdup(char *str)
@@ -13,7 +12,7 @@ char *_strdup(char *str)
 	int i, j;
 
 	if (str == NULL)
-		return(NULL);
+		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
 		i++;
