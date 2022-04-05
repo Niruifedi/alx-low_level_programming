a6dbe52f33bd2be27790c8fc49e1266b950649c2
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-05 01:25:25 -0700

    betty

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 
 	if (width < 1 || height < 1)
 		return (NULL);
-	grid = malloc(sizeof(int*) * height);
+	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
