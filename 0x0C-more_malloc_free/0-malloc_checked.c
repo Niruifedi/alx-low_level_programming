a23c37a9168c0d1387c4357510291021c09443f6
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-07 05:12:46 -0700

    allocate

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates a memory using malloc
+ * @b: size to be allocated
+ * Return: pointer to allocated memory or exit with 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *j;
+
+	j = malloc(sizeof(unsigned int) * b);
+
+	if (j == NULL)
+	{
+		exit(98);
+	}
+
+	return (j);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
+void *malloc_checked(unsigned int b);
+
+#endif
