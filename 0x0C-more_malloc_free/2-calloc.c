012bb739eb10e2b94a4451678241843b5f516c19
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-07 14:06:56 -0700

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - allocates memory in blocks and size of each blocks
+ * @nmemb: size of block
+ * @size: size of element of each block
+ * Return: pointer to the allocated memory.
+ */
+void *_calooc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		*(ptr + i) + 0;
+
+	return ((void *)ptr);
+}
