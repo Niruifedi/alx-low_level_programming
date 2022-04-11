5f6108fb2765c0d46684f51d269b1d6d6b04b22e
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-11 08:56:40 -0700

    file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main - program prints the name of the file it was compiled from
+ * main - prints the name of the file it was compiled from
  * Return: 0
  */
 int main(void)
