8f2cfa98140a81424f4081fa19f92d759c8342ed
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-11 15:07:37 -0700

    struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * @owner: Dog's owner
  * Return: nothing.
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 	{
