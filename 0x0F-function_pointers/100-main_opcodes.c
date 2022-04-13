255f44911a3b16095a0e6fd20d351451434727dd
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-13 13:28:03 -0700

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,11 +1,28 @@
 #include "function_pointers.h"
 
 /**
- * op_sub -
+ *main -  prints the opcodes of its own main function.
+ *@argc: integer value.
+ *@argv: character value.
  *
- * Return: 
+ *Return: 0(success)
  */
-int op_sub(int a, int b)
+int main(int argc, char *argv[])
 {
+	int i;
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < atoi(argv[1]) - 1; i++)
+		printf("%02hhx ", ((char *)main)[i]);
+	printf("%02hhx\n", ((char *)main)[i]);
+	return (0);
 }
