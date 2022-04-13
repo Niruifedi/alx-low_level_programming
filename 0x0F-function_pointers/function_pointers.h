763829e4042f378ed7e0dfac32f2ccbe75c01b2e
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-13 10:23:14 -0700

    header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
 #ifndef HEADERGUARD
 #define HEARDERGUARD
 
-void print_name(char *name, void (*f) (char *));
-void array_iterator(intr *array, size_t size, void (*action)(int));
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 
