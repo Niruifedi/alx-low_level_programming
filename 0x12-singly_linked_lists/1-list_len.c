23584a7f3080f40b40533f9e6ca2ac505c9ad575
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-21 16:00:12 -0700

    list lent

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists_h"
+#include <stddef.h>
+#include<stdlib.h>
+
+/**
+ * list_len - finds the number of node in a list
+ * @h: singly linked list
+ * Return: number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
+}
