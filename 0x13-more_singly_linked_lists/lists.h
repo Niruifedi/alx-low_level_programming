0ea92f94b0148661922e07b76e58a4d3ee86e1bb
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-25 18:57:06 -0700

    linked header

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,7 +21,7 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_node_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
