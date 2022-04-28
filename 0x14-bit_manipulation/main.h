fa83c8903f1963d36bcef4bff0cc2b0385e1bdc9
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-04-28 16:25:26 -0700

    bit man

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
 int _atoi(const char *s);
-
+int get_endianness(void);
 
 #endif
