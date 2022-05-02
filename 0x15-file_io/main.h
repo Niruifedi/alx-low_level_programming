7860c84d2d50c81b7f05ab3e8cb49295bc4ad8ee
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-05-02 15:02:29 -0700

    file header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,10 +3,14 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 
 #endif
