668dc4160a180348f8529e39996955b1d0ab0864
Niruifedi <ifediniruizuchukwu@gmail.com>
2022-10-07 01:28:58 +0100

    jump

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,8 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void pri_arr(int *array, size_t start, size_t end);
+int jump_search(int *array, size_t size, int value);
 
 #endif
 
